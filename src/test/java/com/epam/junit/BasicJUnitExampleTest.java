2306469ae1a67385096e6c955f6943fe93a8b737
Csaba Valyi <csaba_valyi@epam.com>
2013-10-30 11:24:03 +0100

    more Unit testing

--- src/test/java/com/epam/junit/BasicJUnitExampleTest.java
+++ src/test/java/com/epam/junit/BasicJUnitExampleTest.java
@@ -1,8 +1,63 @@
 package com.epam.junit;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 
 
 public class BasicJUnitExampleTest {
+    private BasicJUnitExample basicjunitexample;
     
+    @Before
+    public void setUp(){
+        basicjunitexample = new BasicJUnitExample();
+    }
     
+    @Test
+    public void testUltimateAnswer(){
+        //GIVEN
+        Integer expected = Integer.valueOf(42);
+        //WHEN
+        Integer result = basicjunitexample.ultimateAnswer();
+        //THEN
+        Assert.assertEquals(expected, result);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testBadAnswerShouldThrowExceptionWhenInputEqualsUltimateAnswer(){
+        //GIVEN
+        Integer input = Integer.valueOf(42);
+        //THEN
+        basicjunitexample.badAnswer(input);
+    }
+    
+    @Test
+    public void testBadAnswerShouldNotThrowExceptionWhenInputDoesNotEqualsUltimateAnswer(){
+        //GIVEN
+        Integer input = Integer.valueOf(41);
+        //WHEN
+        Integer result = basicjunitexample.badAnswer(input);
+        //THEN
+        Assert.assertEquals(input, result);
+    }
+    @Test
+    public void isTheUltimateAnswerWhenInputEqualsUltimateAnswer(){
+        //GIVEN
+        Integer input = Integer.valueOf(42);
+        //WHEN
+        Boolean result = basicjunitexample.isTheUltimateAnswer(input);
+        //THEN
+        Assert.assertEquals(result, Boolean.TRUE);
+    }
+    
+    @Test
+    public void isTheUltimateAnswerWhenInputDoesNotEqualUltimateAnswer(){
+        //GIVEN
+        Integer input = Integer.valueOf(41);
+        //WHEN
+        Boolean result = basicjunitexample.isTheUltimateAnswer(input);
+        //THEN
+        Assert.assertEquals(result, Boolean.FALSE);
+    }
 }
